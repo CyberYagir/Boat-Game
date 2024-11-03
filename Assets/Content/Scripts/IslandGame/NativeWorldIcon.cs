3d23e4684ee5fb92c9beddf23834bbe535667407
Yagir <76903670+CyberYagir@users.noreply.github.com>
2024-11-04 01:02:15 +0300

    Shaman Icon UI

--- Assets/Content/Scripts/IslandGame/NativeWorldIcon.cs
+++ Assets/Content/Scripts/IslandGame/NativeWorldIcon.cs
@@ -1,4 +1,5 @@
 using System;
+using Content.Scripts.BoatGame;
 using Content.Scripts.BoatGame.Services;
 using UnityEngine;
 using Zenject;
@@ -7,16 +8,44 @@ namespace Content.Scripts.IslandGame
 {
     public class NativeWorldIcon : MonoBehaviour
     {
+        [SerializeField] private Transform part;
+        [SerializeField] private Transform point;
         private Camera mainCamera;
+        private SelectionService selectionService;
+
         [Inject]
         private void Construct(SelectionService selectionService)
         {
+            this.selectionService = selectionService;
             mainCamera = selectionService.Camera;
+            
+            selectionService.OnChangeSelectObject += SelectionServiceOnOnChangeSelectObject;
+        }
+
+        private void SelectionServiceOnOnChangeSelectObject(ISelectable obj)
+        {
+            
+            if (obj == null)
+            {
+                gameObject.SetActive(true);
+                return;
+            }
+            else if (obj.Transform == part)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
         }
 
         private void LateUpdate()
         {
-            transform.LookAt(mainCamera.transform.position);
+            if (selectionService.SelectedObject == null || selectionService.SelectedObject.Transform != part)
+            {
+                transform.position = mainCamera.WorldToScreenPoint(point.transform.position);
+            }
         }
     }
 }
